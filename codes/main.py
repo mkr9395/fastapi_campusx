ce0f99a888825c52b709249e0ae8cdc41ad8c7ea
Mohit Kumar <mkr9395@gmail.com>
2025-09-16 21:09:25 +0530

    https working

--- codes/main.py
+++ codes/main.py
@@ -1,19 +1,38 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
-
+# load data from json file
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+        
+        return data
+    
+    
+    
 # route creation using decoratoe @ using get method
 @app.get("/")
 def hello():
-    return {'messages':'hello World'}
+    return {'messages':'Patient Management System API'}
 
 # to run th code -> uvicorn main:app --reload -> here main is file name
 
 # another route
 @app.get('/about')
 def about():
-    return {'messages':'This is the first FASTAPI app'}
+    return {'messages':'A fully functional API to manage your patient record'}
+
+# new endpoint to give all the data to client
+@app.get('/view')
+def view():
+    data = load_data()
+    
+    return data
+
+
+
 
 
 
